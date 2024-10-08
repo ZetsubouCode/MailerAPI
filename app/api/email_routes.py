1e901f21dd590dba882afaabc0ec5422423fc3c2
ZetsubouCode <kignatius80@gmail.com>
2024-10-08 14:15:05 +0700

    update route fields

--- app/api/email_routes.py
+++ app/api/email_routes.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, BackgroundTasks, HTTPException
-from app.models.email import EmailRequest
+from fastapi import APIRouter, BackgroundTasks, UploadFile,File
+from app.model.email import EmailRequest
 from app.services.email_service import send_email
 
 router = APIRouter()
 
 @router.post("/send-email/")
-async def send_email_route(email: EmailRequest, background_tasks: BackgroundTasks):
+async def send_email_route(email: EmailRequest, background_tasks: BackgroundTasks,attachment: UploadFile = File(None)):
     try:
         # Add email sending to background tasks
-        background_tasks.add_task(send_email, email.to_email, email.subject, email.body)
-        return {"message": "Email is being sent in the background!"}
+        background_tasks.add_task(send_email, email.to_email, email.subject, email.body,email.cc_emails,email.bcc_emails,attachment)
+        return {"status":False,"message": "Email is being sent in the background!"}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return {"status":False,"message": f"Exception {e}"}
